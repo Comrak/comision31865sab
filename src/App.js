3c371f4378455055feabafd3a58a1abfffc425e8
Comrak <jnardulli@hotmail.com>
2022-05-14 16:35:32 -0300

    cambio de nombre y algunas edicione que hice por jugar un poco

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import logo from './pizzalogot.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>La Vera Pizza</h1>
+        <h1>VERITAS PIZZERIA</h1>
+        <p>La Vera Pizza</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          muy pronto podras ordenar la verdadera pizza italiana por aca 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
